df7323872251c95089be20134352b8a3dcfb488d
Igor Almeida <igor.almeida@evtit.com>
2024-09-16 22:14:10 -0300

    feat: add endpoint to confirm guests invites

--- src/routes/guests/index.ts
+++ src/routes/guests/index.ts
@@ -1,17 +1,36 @@
 import { Request, Response, Router } from "express";
-import { getGuest } from "../../services/guests";
+import { getGuest, markResponseAsync } from "../../services/guests";
 
 const router = Router();
 
-router.get('/:id', async(req: Request, res: Response) => {
+router.get('/:id', async (req: Request, res: Response) => {
   const guestId = req.params.id;
   const guestDetails = await getGuest(guestId);
-  
-  if (!guestDetails){
-    return res.status(404);
+
+  if (!guestDetails) {
+    return res.status(404).send();
   }
 
-  return res.json(guestDetails);
+  return res.json({
+    id: guestDetails.id,
+    confirmed: guestDetails.confirmed,
+    name: guestDetails.name
+  });
+});
+
+router.patch('/:id', async (req: Request, res: Response) => {
+  const id = req.params.id;
+
+  const { confirmed, password } = req.body;
+  const guestDetails = await markResponseAsync(id, password, confirmed);
+
+  if (!guestDetails) {
+    res.status(404).send();
+    return; 
+  }
+
+  res.status(204).send();
+  return; 
 });
 
 export default router;
--- src/services/guests.ts
+++ src/services/guests.ts
@@ -1,4 +1,4 @@
-import { addDoc, getDocs, query, where } from "firebase/firestore/lite";
+import { addDoc, getDocs, query, updateDoc, where } from "firebase/firestore/lite";
 import { guestsCollection } from "./firebase";
 import { GuestType } from "../types";
 
@@ -15,13 +15,30 @@ export const getGuest = async (userId: string) => {
 
   if (!payer.empty)
     return payer.docs[0].data() as GuestType;
-} 
+}
+
+export const markResponseAsync = async (userId: string, password: string, confirmed: boolean) => {
+  const payerQuery = query(
+    guestsCollection,
+    where("id", "==", userId),
+    where("password", "==", password)
+  );
+  
+  const querySnap = await getDocs(payerQuery);
+  
+  if (!querySnap.empty) {
+    const guestRef = querySnap.docs[0].ref;
+
+    await updateDoc(guestRef, { confirmed });
+    return true;
+  }
+  return false;
+}
 
 export const addNewGuest = async (name: string) => {
   try {
     await addDoc(guestsCollection, {
       name,
-      age: 22
     });
   } catch (e) {
     console.error("Error adding document: ", e);
