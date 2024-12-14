8c703018e08197e28ca5fbead5a29b06483a5859
Igor Almeida <igor.almeida@evtit.com>
2024-12-14 08:51:38 -0300

    task: compress content to improve performance to render images

--- src/services/gifts.ts
+++ src/services/gifts.ts
@@ -3,28 +3,57 @@ import { giftsCollection, storage } from "./firebase";
 import { GiftType } from "../types";
 import { v4 as uuidv4 } from 'uuid';
 import { getBytes, ref } from "firebase/storage";
-
+import sharp from "sharp";
 
 const giftsCached: GiftType[] = [];
 
 export const getAllgifts = async () => {
   if (giftsCached.length === 0) {
     const giftsSnap = await getDocs(giftsCollection);
-    const result = giftsSnap.docs.map(item => item.data()) as GiftType[];
-    result.forEach((item: GiftType) => {
-      const fileReference = ref(storage, item.image!);
+    const gifts = giftsSnap.docs.map(item => item.data()) as GiftType[];
 
-      getBytes(fileReference).then((imageBuffer: ArrayBuffer) => {
-        const base64 = Buffer.from(imageBuffer).toString('base64');
+    const processedGifts = await Promise.all(
+      gifts.map(async (item: GiftType) => {
+        try {
+          const fileReference = ref(storage, item.image!);
 
-        item.image = `data:image/jpeg;base64,${base64}`;
-      });
-      return giftsCached.push(item)
-    }
+          const imageBuffer = await getBytes(fileReference);
+          const compressed = await compressImage(imageBuffer);
+          return {
+            ...item,
+            image: `data:image/jpeg;base64,${compressed?.toString('base64')}`
+          };
+
+        } catch (error) {
+          console.error(`Error processing image ${item.name}:`, error);
+          return item;
+        }
+      })
     );
+
+    giftsCached.push(...processedGifts);
   }
 
   return giftsCached;
+}
+
+async function compressImage(imagePath: ArrayBuffer) {
+  try {
+    const formatted = new Uint8Array(imagePath);
+    const outputBuffer = await sharp(formatted)
+      .resize({
+        width: 1920,
+        height: 1080,
+        fit: sharp.fit.inside,
+        withoutEnlargement: true,
+      })
+      .webp({ quality: 80 })
+      .toBuffer();
+
+      return outputBuffer;
+  } catch (error) {
+    console.error('Erro on processing image:', error);
+  }
 }
 
 export const addGifts = async (payload: GiftType) => {
