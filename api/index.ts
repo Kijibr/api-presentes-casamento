99570a848877e35e58b7b0cf270cac40aa30a28f
Igor Almeida <igoreted@gmail.com>
2025-05-10 03:27:35 -0300

    refactor: migrate API entry point to TypeScript and update related configurations

---
+++ api/index.ts
@@ -0,0 +1,39 @@
+import express, { Request, Response } from "express";
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import cors from 'cors';
+import { appConfig } from "../dist/config/keys";
+import routes from "../dist/routes";
+import { LogInformation } from "../dist/services/logger";
+
+const app = express();
+
+const PORT = appConfig.appPort || '5005';
+app.use(cors({
+  origin: appConfig.appUrl
+}));
+
+app.use(express.json({
+  limit: '10mb'
+}));
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use((req: Request, res: Response, next: Function) => {
+  LogInformation(`Request: ${req.method} ${req.url}`);
+  next();
+});
+
+app.get('/', (req: Request, res: Response) => res.send('api casamento is running, ok!'));
+
+app.use('/api', routes);
+
+app.listen(PORT, () => {
+  LogInformation("Server running at PORT: " + PORT);
+}).on("error", (error) => {
+  throw new Error(error.message);
+});
+
+export default app;
