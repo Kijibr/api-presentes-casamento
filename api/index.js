b5a63d0d48c42c91926d28b0b6724bbdbc13d040
Igor Almeida <igoreted@gmail.com>
2025-05-10 03:46:38 -0300

    add serverless lib

--- api/index.js
+++ api/index.js
@@ -1,2 +1,5 @@
-const app = require('../dist/index.js');
-module.exports = app.default || app;
+// api/index.js (na raiz do projeto)
+const serverless = require('serverless-http');
+const { handler } = require('../dist/index.js');
+
+module.exports.handler = serverless(handler);
--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,42 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response } from 'express';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import cors from 'cors';
-import { LogInformation } from "./services/logger";
-import { appConfig } from "./config/keys";
-import routes from "./routes";
+import { LogInformation } from './services/logger';
+import { appConfig } from './config/keys';
+import routes from './routes';
 
 const app = express();
+const PORT = appConfig.appPort || 5005;
 
-const PORT = appConfig.appPort || '5005';
+// Configurações do CORS
 app.use(cors({
-  origin: appConfig.appUrl
+  origin: appConfig.appUrl,
 }));
 
-app.use(express.json({
-  limit: '10mb'
-}));
+// Configurações de middleware
+app.use(express.json({ limit: '10mb' }));
 app.use(logger('dev'));
-app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+// Logger para cada requisição
 app.use((req: Request, res: Response, next: Function) => {
   LogInformation(`Request: ${req.method} ${req.url}`);
   next();
 });
 
-console.log('inicializado API')
+// Rota de teste
 app.get('/', (req: Request, res: Response) => {
-  res.send('api casamento is running, ok!')
+  res.send('API casamento is running, ok!');
 });
 
+// Rotas da API
 app.use('/api', routes);
 
-app.listen(PORT, () => {
-  LogInformation("Server running at PORT: " + PORT);
-}).on("error", (error) => {
-  throw new Error(error.message);
-});
-
-export default app;
+// Exportando o handler para uso com serverless-http (para Vercel)
+if (process.env.NODE_ENV !== 'production') {  // Quando rodar localmente, usamos o Express normalmente
+  app.listen(PORT, () => {
+    LogInformation(`Server running at http://localhost:${PORT}`);
+  });
+}
